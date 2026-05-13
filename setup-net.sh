311b7d47348aaf59a08b70258c34f6df9d903922
1mctrl <ibx.icon@mail.ru>
2026-05-13 23:21:43 +0500

    fiiiiix net

--- setup-net.sh
+++ setup-net.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -e
 
-declare -A BRIDGES=(
-    [br-host-gw]="10.0.0.254/30"
-    [br-gw-r1]=""
-    [br-gw-r2]=""
-    [br-r1-c1]=""
-    [br-r1-c2]=""
-    [br-r2-c3]=""
-)
-
 create_bridge() {
     local BR="$1" ADDR="$2"
     ip link show "$BR" &>/dev/null || { ip link add name "$BR" type bridge && ip link set "$BR" up; }
@@ -21,7 +12,12 @@ create_tap() {
     ip link show "$TAP" &>/dev/null || { ip tuntap add dev "$TAP" mode tap && ip link set "$TAP" up && ip link set "$TAP" master "$BR"; }
 }
 
-for BR in "${!BRIDGES[@]}"; do create_bridge "$BR" "${BRIDGES[$BR]}"; done
+create_bridge br-host-gw "10.0.0.254/30"
+create_bridge br-gw-r1   ""
+create_bridge br-gw-r2   ""
+create_bridge br-r1-c1   ""
+create_bridge br-r1-c2   ""
+create_bridge br-r2-c3   ""
 
 # gw tapochki
 create_tap tap-gw-host br-host-gw
@@ -42,7 +38,6 @@ create_tap tap-c3      br-r2-c3
 sysctl -qw net.ipv4.ip_forward=1
 grep -q "net.ipv4.ip_forward=1" /etc/sysctl.conf || echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
 
-# (default: wlan0)
 UPLINK="${1:-wlan0}"
 iptables -t nat -C POSTROUTING -s 10.0.0.0/8 -o "$UPLINK" -j MASQUERADE 2>/dev/null \
     || iptables -t nat -A POSTROUTING -s 10.0.0.0/8 -o "$UPLINK" -j MASQUERADE
